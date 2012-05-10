a40d0654192a36a199e53e8817512c7d92fb5145
Louis Acresti <louis.acresti@gmail.com>
2012-05-10 03:15:35 -0400

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cake build
+mkdir -p deploy
+cp -r swf *.js *.html *.css *.png deploy/.
+
+git checkout gh-pages
+mv deploy/* .
+git add *.js *.html *.css *.png
+
+rm -rf deploy
+git commit -am 'auto-deploy'
+git push origin gh-pages
+
+git checkout master
